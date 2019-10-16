97198157094b1fe735df24708f4aef278b2a63c7
TomokazuShimada <pikkoro.dreamers.high.tmkz@gmail.com>
2019-10-16 23:43:43 +0000

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
-  
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -18,8 +18,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user # 保存成功後、ログインします。
-      flash[:success] = '作成に成功しました。'
+      log_in @user
+      flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
       render :new
@@ -31,24 +31,40 @@ class UsersController < ApplicationController
 
   def update
     if @user.update_attributes(user_params)
-      flash[:success] = "ユーザー情報を編集しました。"
+      flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
-      render :edit
+      render :edit      
     end
   end
-  
+
   def destroy
     @user.destroy
     flash[:success] = "#{@user.name}のデータを削除しました。"
     redirect_to users_url
   end
 
+  def edit_basic_info
+  end
+
+  def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+    redirect_to users_url
+  end
+
   private
 
-      def user_params
+    def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
-      end
+    end
+    
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
+    end
 
     # beforeフィルター
 
@@ -70,7 +86,7 @@ class UsersController < ApplicationController
     def correct_user
       redirect_to(root_url) unless current_user?(@user)
     end
-    
+
     # システム管理権限所有かどうか判定します。
     def admin_user
       redirect_to root_url unless current_user.admin?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191015101408) do
+ActiveRecord::Schema.define(version: 20191016213513) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20191015101408) do
     t.string "department"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.datetime "basic_time", default: "2019-10-16 23:00:00"
+    t.datetime "work_time", default: "2019-10-16 22:00:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
