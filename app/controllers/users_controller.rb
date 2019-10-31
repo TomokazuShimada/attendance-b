dca2547362a16639dffd851f7fe2584104d35639
EC2 Default User <ec2-user@ip-172-31-46-30.ap-northeast-1.compute.internal>
2019-10-31 14:59:59 +0000

    Final Kintai

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,10 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:index, :destroy, :edit_basic_info, :update_basic_info]
   before_action :set_one_month, only: :show
+  before_action :admin_or_correct_user, only: :show
 
   def index
     @users = User.paginate(page: params[:page]).search(params[:search])
-  end
-  
-  def search
-    @users = User.search(page: params[:page]).(params[:search])
   end
 
   def show
@@ -73,5 +70,13 @@ class UsersController < ApplicationController
 
     def basic_info_params
       params.require(:user).permit(:department, :basic_time, :work_time)
+    end
+    
+    def admin_or_correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集権限がありません。"
+        redirect_to(root_url)
+      end
     end
 end
